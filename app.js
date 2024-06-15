01044cadec4863c0a7a972f53ff0a83a4e44576d
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-06-15 21:05:56 +0300

    add get all users route

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
-// Impelement Get All Tours route
+// getting the users data
+const users = require("./users");
+
+//  Get All users  route
+app.get("/api/users", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      users: users,
+    },
+  });
+});
 
 app.listen(3000, () => {
   console.log("the server is listening on port 300 ");
