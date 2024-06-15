7f8083501dc0321429f8aee1f8ffc86335d6e454
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-06-15 18:09:30 +0300

    project setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+// Impelement Get All Tours route
+
+app.listen(3000, () => {
+  console.log("the server is listening on port 300 ");
+});
