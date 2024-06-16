6cc9ab3c354cc36755096b2b67ed5311bb189277
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-06-16 17:05:40 +0300

    refactor our routes

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
 const express = require("express");
-const fs = require("fs");
 const app = express();
 
 // getting the users data
 const users = require("./users");
 
-//  Get All users  route
-app.get("/api/users", (req, res) => {
+const getAllUsers = (req, res) => {
   res.status(200).json({
     status: "success",
     data: {
-      users: users,
+      users: "<list of users>",
     },
   });
-});
+};
+
+const getSingleUser = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      users: "<single user>",
+    },
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(201).json({
+    status: "success",
+    data: {
+      users: "<new user>",
+    },
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      users: "<updated user>",
+    },
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      users: "<deleted user>",
+    },
+  });
+};
+
+app.route("/api/users").get(getAllUsers).post(createUser);
+
+app
+  .route("/api/users/:id")
+  .get(getSingleUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 app.listen(3000, () => {
-  console.log("the server is listening on port 300 ");
+  console.log("the server is listening on port 3000 ");
 });
--- users.js
+++ users.js
@@ -4,30 +4,35 @@ const users = [
     fullname: "John Doe",
     plainPassword: "password123",
     email: "john.doe@example.com",
+    photo: "john_doe.jpg",
   },
   {
     id: 2,
     fullname: "Jane Smith",
     plainPassword: "securepass456",
     email: "jane.smith@example.com",
+    photo: "jane_smith.jpg",
   },
   {
     id: 3,
     fullname: "Michael Brown",
     plainPassword: "mypassword789",
     email: "michael.brown@example.com",
+    photo: "michael_brown.jpg",
   },
   {
     id: 4,
     fullname: "Emily Davis",
     plainPassword: "passw0rd",
     email: "emily.davis@example.com",
+    photo: "emily_davis.jpg",
   },
   {
     id: 5,
     fullname: "David Wilson",
     plainPassword: "password321",
     email: "david.wilson@example.com",
+    photo: "david_wilson.jpg",
   },
 ];
 
